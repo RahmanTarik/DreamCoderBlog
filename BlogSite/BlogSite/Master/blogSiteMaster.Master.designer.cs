2ac2ca82459acf5aff70946fb6efe28cc7a170ec
aAnanna <ananna.rahman0309@gmail.com>
2015-06-05 16:34:19 +0600

    Restore final

--- BlogSite/BlogSite/Master/blogSiteMaster.Master.designer.cs
+++ BlogSite/BlogSite/Master/blogSiteMaster.Master.designer.cs
@@ -31,24 +31,6 @@ namespace BlogSite {
         protected global::System.Web.UI.HtmlControls.HtmlForm masterForm;
         
         /// <summary>
-        /// signInButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button signInButton;
-        
-        /// <summary>
-        /// signUpButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button signUpButton;
-        
-        /// <summary>
         /// Container control.
         /// </summary>
         /// <remarks>
