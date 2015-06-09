f05b181162f44c59586138e86d55e90d4c2bf7c1
yeasinmahi <yeasinmahi72@gmail.com>
2015-06-09 19:49:13 +0600

    Sql Injection Solve

--- BlogSite/BlogSite/DAL/BlogerGatway.cs
+++ BlogSite/BlogSite/DAL/BlogerGatway.cs
@@ -15,8 +15,12 @@ namespace BlogSite.DAL
         public int InsertPost(Post post)
         {
             SqlConnection connection = new SqlConnection(_connectionString);
-            string query = "insert into post (title,post,status,uid) values ('"+post.PostTitle+"','"+post.PostBody+"','"+post.PostStatus+"',"+post.Uid+")";
+            string query = "insert into post (title,post,status,uid) values (@title,@post,@status,@uid)";
             SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.Add("@title", post.PostTitle);
+            command.Parameters.Add("@post", post.PostBody);
+            command.Parameters.Add("@status", post.PostStatus);
+            command.Parameters.Add("@uid", post.Uid);
             connection.Open();
             int rowAffected = command.ExecuteNonQuery();
             connection.Close();
